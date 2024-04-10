b365df6b51f3d0a31da3dbb1b46d48e38843f94e
Raghav Garg <raghav.garg@somaiya.edu>
2024-04-10 12:44:08 +0530

    Minor changes

--- register.php
+++ register.php
@@ -99,6 +99,32 @@
 
         <button type="submit">Register</button>
       </form>
+      <?php
+    // Error messages
+    if (isset($_GET["error"])) {
+      if ($_GET["error"] == "emptyinput") {
+        echo "<p>Fill in all fields!</p>";
+      }
+      else if ($_GET["error"] == "invaliduid") {
+        echo "<p>Choose a proper username!</p>";
+      }
+      else if ($_GET["error"] == "invalidemail") {
+        echo "<p>Choose a proper email!</p>";
+      }
+      else if ($_GET["error"] == "passwordsdontmatch") {
+        echo "<p>Passwords doesn't match!</p>";
+      }
+      else if ($_GET["error"] == "stmtfailed") {
+        echo "<p>Something went wrong!</p>";
+      }
+      else if ($_GET["error"] == "usernametaken") {
+        echo "<p>Username already taken!</p>";
+      }
+      else if ($_GET["error"] == "none") {
+        echo "<p>You have signed up!</p>";
+      }
+    }
+  ?>
 
       
       <div class="additional-links">
